0ca5f019c9912a213cf76971da6fa8e5a1f87903
Felipe <fedantas33@gmail.com>
2026-09-17 17:06:44 -0300

    feat: adiciona imagem, botão e ajustes no cadastro

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+function cadastrarVinho() {
 var nomeVinho = prompt("Digite o nome do vinho: ");
 
 var tipoVinho = prompt("Digite o tipo de vinho (tinto, branco, rosé): ");
@@ -5,12 +6,16 @@ var tipoVinho = prompt("Digite o tipo de vinho (tinto, branco, rosé): ");
 var safraVinho = parseInt(prompt("Digite a safra do vinho: "));
 
 var quantidadeEstoque = parseInt(prompt("Digite a quantidade em estoque: "));
+var novasGarrafas = 10;
+var novoEstoque = quantidadeEstoque + novasGarrafas;
+
+console.log("Novo estoque: " + novoEstoque);
 
 var precoVinho = parseFloat(prompt("Digite o preço do vinho: "));
 
-alert("Cadastro realizado! Veja os detalhes no console.");
+var totalEstoque = quantidadeEstoque * precoVinho;
 
-alert("A seguir, veja os detalhes do vinho no console.");
+alert("Cadastro realizado! Veja os detalhes no console.");
 
 
 console.log("Nome do vinho: " + nomeVinho);
@@ -18,3 +23,6 @@ console.log("Tipo de vinho: " + tipoVinho);
 console.log("Safra do vinho: " + safraVinho);
 console.log("Quantidade em estoque: " + quantidadeEstoque);
 console.log("Preço do vinho: R$ " + precoVinho.toFixed(2));
+
+console.log("Valor total em estoque: R$ " + totalEstoque.toFixed(2));
+}
